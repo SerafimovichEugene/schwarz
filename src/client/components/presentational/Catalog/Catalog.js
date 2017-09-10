67234093a90bd5228e198c51a4295e4e4d617d17
SerafimovichEugene <eugene.serafimovich@gmail.com>
2017-09-10 19:00:50 +0300

    ES: add propTypes to Catalog component

--- src/client/components/presentational/Catalog/Catalog.js
+++ src/client/components/presentational/Catalog/Catalog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import CircularProgress from 'material-ui/CircularProgress';
 import MainAppComponent from '../../containers/MainAppComponentContainer/MainAppComponentContainer';
 import CardList from './CardList/CardList';
@@ -10,20 +11,28 @@ import './Catalog.scss';
 const circularStyle = {
     position: 'absolute',
     top: '50%',
-    left: '50%'
+    left: '50%',
 }
 
 export default class Catalog extends Component {
+
+    static propTypes = {
+        fetchProducts: PropTypes.any.isRequired,
+        query: PropTypes.any.isRequired,
+        isLoading: PropTypes.bool.isRequired,
+        products: PropTypes.any.isRequired,
+    }    
+
     constructor(props) {
         super(props);
         this.state = {
-            query: '?count=15&currency=RUB'
+            query: '?count=15&currency=RUB',
         }
     }
 
     handlePageChange = (page) => {
         const { fetchProducts } = this.props;
-        let query = this.state.query.concat(`&page=${page}`);
+        const query = this.state.query.concat(`&page=${page}`);
         fetchProducts(query);
         this.setState({
             currentPage: page,
@@ -44,16 +53,16 @@ export default class Catalog extends Component {
         if(isLoading) {
             console.log('START FETCHING IS LOADING');
             return <CircularProgress size={80} style={circularStyle} />
-        } else {
-            let { products } = this.props;
-            if(products.data) {
-                return <CardList
-                    currentPage={this.state.currentPage}
-                    onPageChange={this.handlePageChange}
-                    products={products.data}
-                    pages={products.pages} />
-            }
+        } 
+        const { products } = this.props;
+        if(products.data) {
+            return <CardList
+                currentPage={this.state.currentPage}
+                onPageChange={this.handlePageChange}
+                products={products.data}
+                pages={products.pages} />
         }
+        
     }
     render() {
         return (
