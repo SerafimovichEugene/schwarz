fbd3dee6169660d2d45c66460d7b7838d5ccc7e4
Serafimovich Eugene <Eugene.serafimovich@gmail.com>
2017-09-13 16:06:01 +0300

    ES: intemidiate commit, toggle by js

--- src/client/components/presentational/NavBar/NavBar.js
+++ src/client/components/presentational/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { WindowResizeListener } from 'react-window-resize-listener'
 // import classNames from 'classnames';
 import UserBar from '../../containers/UserBarContainer/UserBarContainer';
 import './NavBar.scss';
@@ -9,6 +10,17 @@ export default class NavBar extends Component {
 
     static propTypes = {        
         user: PropTypes.any.isRequired,      
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            isToggleMenu: false,
+            windowWidth: 0,     
+        };
+        this.handleToggleMenu = this.handleToggleMenu.bind(this);
+    }
+    handleToggleMenu() {
+        this.setState({ isToggleMenu: !this.state.isToggleMenu });
     }
 
     renderUserBar = (user) => {
@@ -26,18 +38,32 @@ export default class NavBar extends Component {
     render() {
         const { user } = this.props;
         return (
-            <header className='nav-bar'>                
+            <header className='nav-bar'>
+                <WindowResizeListener onResize={windowSize => {
+                    if(windowSize.windowWidth > 768 && this.state.isToggleMenu) {
+                        this.handleToggleMenu();
+                    }
+                    if(windowSize.windowWidth < 768) {
+                        this.handleToggleMenu();
+                    }                    
+                }}/>
                 <Link to='/' className="brand">SCHWARZ</Link>
                 <div className='collapse-menu'>
-                    <div><i className="fa fa-bars fa-2x" aria-hidden="true" /></div>
-                    <div className='menu-items'/>
+                    <div        
+                        className='menu-icon' 
+                        onClick={this.handleToggleMenu}><i className="fa fa-bars fa-2x" aria-hidden="true" />
+                    </div>
+                    <ul
+                        id={this.state.isToggleMenu ? 'menu-hide' : ''}
+                        className='menu-items'>                    
+                        <li><Link to='/catalog' className="catalog-link">Kаталог</Link></li>
+                        <li><Link to='/' className="">Контакты</Link></li>
+                        <li><Link to='/' className="">О нас</Link></li>
+                    </ul>
                 </div>
-                <ul className='menu'>                    
-                    <li><Link to='/catalog' className="catalog-link">Kаталог</Link></li>
-                    <li><Link to='/' className="">Контакты</Link></li>
-                    <li><Link to='/' className="">О нас</Link></li>
-                </ul>
+
                 {this.renderUserBar(user)}
+
             </header>
         )
     }
